d38061a94eba6d0a24c8716136a5e92255115f6a
Daniel Streit <Daniel.Streit@gmail.com>
2014-06-05 16:55:18 -0700

    Implement reduce

--- src/underbar.js
+++ src/underbar.js
@@ -163,6 +163,13 @@ var _ = {};
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator === undefined) {
+      accumulator = _.first(collection);
+    }
+    _.each(collection, function(item) {
+      accumulator = iterator(accumulator, item);
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
